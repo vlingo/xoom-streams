3468406722bf37fe6752b92b40a371f73791cb7e
Kevin Mas Ruiz <masruizkevin@gmail.com>
2019-12-06 19:53:19 +0100

    Initial commit

--- src/main/java/io/vlingo/pipes/http/HttpStreamServer.java
+++ src/main/java/io/vlingo/pipes/http/HttpStreamServer.java
@@ -1,3 +1,10 @@
+// Copyright © 2012-2019 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.pipes.http;
 
 import io.vlingo.actors.Stage;
--- src/main/java/io/vlingo/pipes/operator/Delay.java
+++ src/main/java/io/vlingo/pipes/operator/Delay.java
@@ -1,5 +1,12 @@
 package io.vlingo.pipes.operator;
 
+// Copyright © 2012-2019 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 import io.vlingo.pipes.Record;
 
 import java.util.Queue;
