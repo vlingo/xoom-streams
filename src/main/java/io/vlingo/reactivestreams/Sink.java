57f0e59f90ab622ea223a0b3f2e1ab490e7eeaf0
VaughnVernon <vaughn@forcomprehension.com>
2019-12-30 11:34:50 -0700

    Javadoc.

--- src/main/java/io/vlingo/reactivestreams/Sink.java
+++ src/main/java/io/vlingo/reactivestreams/Sink.java
@@ -18,24 +18,57 @@ import io.vlingo.reactivestreams.sink.PrintSink;
  * @param <T> the type of the streamed values
  */
 public interface Sink<T> {
+  /**
+   * Answer a new {@code Sink<T>} that relays to the {@code consumer}.
+   * @param consumer the {@code Consumer<T>} that will consume values
+   * @param <T> the type of values to be consumed
+   * @return {@code Sink<T>}
+   */
   public static <T> Sink<T> consumeWith(final Consumer<T> consumer) {
     return new ConsumerSink<>(consumer);
   }
 
+  /**
+   * Answer a new {@code Sink<T>} that prints to the standard output
+   * stream where each line starts with {@code prefix}.
+   * @param prefix the String that begins each line printed
+   * @param <T> the type of values to be printed
+   * @return {@code Sink<T>}
+   */
   public static <T> Sink<T> printToStdout(final String prefix) {
     return printTo(System.out, prefix);
   }
 
+  /**
+   * Answer a new {@code Sink<T>} that prints to the standard error
+   * stream where each line starts with {@code prefix}.
+   * @param prefix the String that begins each line printed
+   * @param <T> the type of values to be printed
+   * @return {@code Sink<T>}
+   */
   public static <T> Sink<T> printToStderr(final String prefix) {
     return printTo(System.err, prefix);
   }
 
+  /**
+   * Answer a new {@code Sink<T>} that prints to the given {@code printStream}
+   * where each line starts with {@code prefix}.
+   * @param printStream the {@code PrintStream} where output values are printed
+   * @param prefix the String that begins each line printed
+   * @param <T> the type of values to be printed
+   * @return {@code Sink<T>}
+   */
   public static <T> Sink<T> printTo(final PrintStream printStream, final String prefix) {
     return new PrintSink<>(printStream, prefix);
   }
 
   /**
-   * Receives the indication to terminate.
+   * Indicates that the receiver should become ready to receive values.
+   */
+  void ready();
+
+  /**
+   * Indicates that the receiver has been terminated.
    */
   void terminate();
 
