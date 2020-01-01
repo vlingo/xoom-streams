657e7704202f057b39e06f18948095d3f68741e4
VaughnVernon <vaughn@forcomprehension.com>
2020-01-01 11:50:46 -0700

    Initial revision of Transformer.

---
+++ src/main/java/io/vlingo/reactivestreams/Transformer.java
@@ -0,0 +1,27 @@
+// Copyright © 2012-2019 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.reactivestreams;
+
+import io.vlingo.common.Completes;
+
+/**
+ * Transform a value of type {@code T} to a value of type {@code R}.
+ *
+ * @param <T> the type of the original value
+ * @param <R> the type of the resulting value
+ *
+ * @see StreamTransformer
+ */
+public interface Transformer<T,R> {
+  /**
+   * Answer the {@code Completes<R>} value after transforming it from the {@code value} of type {@code T}.
+   * @param value the T typed original value
+   * @return {@code Completes<R>}
+   */
+  Completes<R> transform(final T value);
+}
