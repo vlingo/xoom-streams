cb2ea7ef463be44fd956a45d4b0b7362b4cecb8f
VaughnVernon <vaughn@forcomprehension.com>
2020-01-01 11:46:23 -0700

    Factored behavior into StreamSubscriberDelegate.

--- src/main/java/io/vlingo/reactivestreams/StreamSubscriber.java
+++ src/main/java/io/vlingo/reactivestreams/StreamSubscriber.java
@@ -19,66 +19,29 @@ import io.vlingo.actors.Stoppable;
  * @param <T> the type of values consumed
  */
 public class StreamSubscriber<T> extends Actor implements Subscriber<T>, Stoppable {
-  private boolean completed;
-  private long count;
-  private boolean errored;
-  private final long requestThreshold;
-  private final Sink<T> sink;
-  private Subscription subscription;
+  private final Subscriber<T> subscriber;
 
   public StreamSubscriber(final Sink<T> sink, final long requestThreshold) {
-    this.sink = sink;
-    this.requestThreshold = requestThreshold;
-    this.count = 0;
-    this.completed = false;
-    this.errored = false;
+    this.subscriber = new StreamSubscriberDelegate<>(sink, requestThreshold, logger());
   }
 
   @Override
   public void onSubscribe(final Subscription subscription) {
-    if (this.subscription == null) {
-      this.subscription = subscription;
-      this.sink.ready();
-      this.subscription.request(requestThreshold);
-    } else {
-      subscription.cancel();
-    }
+    subscriber.onSubscribe(subscription);
   }
 
   @Override
   public void onNext(final T value) {
-    if (!isFinalized()) {
-      sink.whenValue(value);
-
-      if (++count >= requestThreshold) {
-        subscription.request(requestThreshold);
-      }
-    }
+    subscriber.onNext(value);
   }
 
   @Override
   public void onComplete() {
-    this.completed = true;
-
-    terminate();
-
-    logger().info("Subscriber with " + sink + " is completed.");
+    this.subscriber.onComplete();
   }
 
   @Override
   public void onError(final Throwable cause) {
-    this.errored = true;
-
-    terminate();
-
-    logger().error("Subscriber with " + sink + " is terminating because: " + cause.getMessage(), cause);
-  }
-
-  private boolean isFinalized() {
-    return completed || errored;
-  }
-
-  private void terminate() {
-    sink.terminate();
+    subscriber.onError(cause);
   }
 }
