d83848e0c01fa62fc6a4e6d206b3a321fb843a36
VaughnVernon <vaughn@forcomprehension.com>
2020-01-01 11:52:07 -0700

    Factored StreamSubscriber behavior into StreamSubscriberDelegate.

---
+++ src/main/java/io/vlingo/reactivestreams/StreamSubscriberDelegate.java
@@ -0,0 +1,80 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.reactivestreams;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.vlingo.actors.Logger;
+
+public class StreamSubscriberDelegate<T> implements Subscriber<T> {
+  private boolean completed;
+  private long count;
+  private boolean errored;
+  private final Logger logger;
+  private final long requestThreshold;
+  private final Sink<T> sink;
+  private Subscription subscription;
+
+  public StreamSubscriberDelegate(final Sink<T> sink, final long requestThreshold, final Logger logger) {
+    this.sink = sink;
+    this.requestThreshold = requestThreshold;
+    this.logger = logger;
+    this.count = 0;
+    this.completed = false;
+    this.errored = false;
+  }
+
+  @Override
+  public void onSubscribe(final Subscription subscription) {
+    if (this.subscription == null) {
+      this.subscription = subscription;
+      this.sink.ready();
+      this.subscription.request(requestThreshold);
+    } else {
+      subscription.cancel();
+    }
+  }
+
+  @Override
+  public void onNext(final T value) {
+    if (!isFinalized()) {
+      sink.whenValue(value);
+
+      if (++count >= requestThreshold) {
+        subscription.request(requestThreshold);
+      }
+    }
+  }
+
+  @Override
+  public void onComplete() {
+    this.completed = true;
+
+    terminate();
+
+    logger.info("Subscriber with " + sink + " is completed.");
+  }
+
+  @Override
+  public void onError(final Throwable cause) {
+    this.errored = true;
+
+    terminate();
+
+    logger.error("Subscriber with " + sink + " is terminating because: " + cause.getMessage(), cause);
+  }
+
+  public boolean isFinalized() {
+    return completed || errored;
+  }
+
+  private void terminate() {
+    sink.terminate();
+  }
+}
