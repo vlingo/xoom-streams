15a9bd6e2c8ee40afd19396f78a20ef527209705
VaughnVernon <vaughn@forcomprehension.com>
2020-01-09 01:16:57 -0700

    TCK WIP.

--- vlingo-streams-tck/src/test/java/io/vlingo/reactivestreams/StreamPublisherCompatibilityTest.java
+++ vlingo-streams-tck/src/test/java/io/vlingo/reactivestreams/StreamPublisherCompatibilityTest.java
@@ -7,9 +7,6 @@
 
 package io.vlingo.reactivestreams;
 
-import java.util.stream.Collectors;
-import java.util.stream.LongStream;
-
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.tck.PublisherVerification;
@@ -17,22 +14,28 @@ import org.reactivestreams.tck.TestEnvironment;
 
 import io.vlingo.actors.Definition;
 import io.vlingo.actors.World;
+import io.vlingo.reactivestreams.Streams.OverflowPolicy;
 
 public class StreamPublisherCompatibilityTest extends PublisherVerification<Long> {
+  final World world = World.startWithDefaults("streams");
 
   public StreamPublisherCompatibilityTest() {
-    super(new TestEnvironment());
+    super(new TestEnvironment(2000, 2000, 100, true)); // new TestEnvironment(200, 100, 50, true)
   }
 
   @Override
   @SuppressWarnings("unchecked")
-  public Publisher<Long> createPublisher(long l) {
-    final World world = World.startWithDefaults("streams");
+  public Publisher<Long> createPublisher(final long elements) {
+    final Source<Long> source = Source.rangeOf(1, elements + 1);
 
-    final Source<Long> source = Source.with(LongStream.range(0, l).boxed().collect(Collectors.toList()));
+    final PublisherConfiguration configuration =
+            PublisherConfiguration.with(
+                    PublisherConfiguration.FastestProbeInterval,
+                    Streams.DefaultMaxThrottle,
+                    Streams.DefaultBufferSize,
+                    OverflowPolicy.DropHead);
 
-    final Definition definition = Definition.has(StreamPublisher.class,
-            Definition.parameters(source, PublisherConfiguration.defaultDropHead()));
+    final Definition definition = Definition.has(StreamPublisher.class, Definition.parameters(source, configuration));
 
     final Publisher<Long> publisher = world.actorFor(Publisher.class, definition);
 
@@ -43,14 +46,135 @@ public class StreamPublisherCompatibilityTest extends PublisherVerification<Long
   public Publisher<Long> createFailedPublisher() {
     return new Publisher<Long>() {
       @Override
-      public void subscribe(Subscriber<? super Long> s) {
-        s.onError(new RuntimeException("Can't subscribe subscriber: " + s + ", because of reasons."));
+      public void subscribe(Subscriber<? super Long> subscriber) {
+        // Baffling. This proves nothing about the actual publisher, and shouldn't,
+        // because designing the publisher with some sort of built-in failure
+        // condition would be even more ridiculous than this. And BTW, the
+        // subscriber will always be registered before onError() because there
+        // is no way to signal a subscriber unless it is registered.
+        subscriber.onSubscribe(new SubscriptionController<>(subscriber, null, null));
+        subscriber.onError(new RuntimeException("Can't subscribe subscriber: " + subscriber + ", because of reasons."));
       }
     };
   }
 
-  @Override
-  public long maxElementsFromPublisher() {
-    return 1000;
-  }
+//  @Test
+//  @Override
+//  public void required_createPublisher1MustProduceAStreamOfExactly1Element() throws Throwable {
+//    super.required_createPublisher1MustProduceAStreamOfExactly1Element();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_createPublisher3MustProduceAStreamOfExactly3Elements() throws Throwable {
+//    super.required_createPublisher3MustProduceAStreamOfExactly3Elements();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec101_subscriptionRequestMustResultInTheCorrectNumberOfProducedElements() throws Throwable {
+//    super.required_spec101_subscriptionRequestMustResultInTheCorrectNumberOfProducedElements();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec102_maySignalLessThanRequestedAndTerminateSubscription() throws Throwable {
+//    super.required_spec102_maySignalLessThanRequestedAndTerminateSubscription();
+//  }
+//
+//  @Test
+//  @Override
+//  public void stochastic_spec103_mustSignalOnMethodsSequentially() throws Throwable {
+//    super.stochastic_spec103_mustSignalOnMethodsSequentially();
+//  }
+//
+//  @Test
+//  @Override
+//  public void optional_spec104_mustSignalOnErrorWhenFails() throws Throwable {
+//    super.optional_spec104_mustSignalOnErrorWhenFails();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec105_mustSignalOnCompleteWhenFiniteStreamTerminates() throws Throwable {
+//    super.required_spec105_mustSignalOnCompleteWhenFiniteStreamTerminates();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec107_mustNotEmitFurtherSignalsOnceOnCompleteHasBeenSignalled() throws Throwable {
+//    super.required_spec105_mustSignalOnCompleteWhenFiniteStreamTerminates();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec109_mustIssueOnSubscribeForNonNullSubscriber() throws Throwable {
+//    super.required_spec109_mustIssueOnSubscribeForNonNullSubscriber();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec302_mustAllowSynchronousRequestCallsFromOnNextAndOnSubscribe() throws Throwable {
+//    super.required_spec302_mustAllowSynchronousRequestCallsFromOnNextAndOnSubscribe();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec303_mustNotAllowUnboundedRecursion() throws Throwable {
+//    super.required_spec303_mustNotAllowUnboundedRecursion();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec306_afterSubscriptionIsCancelledRequestMustBeNops() throws Throwable {
+//    super.required_spec306_afterSubscriptionIsCancelledRequestMustBeNops();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec307_afterSubscriptionIsCancelledAdditionalCancelationsMustBeNops() throws Throwable {
+//    super.required_spec307_afterSubscriptionIsCancelledAdditionalCancelationsMustBeNops();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec309_requestNegativeNumberMustSignalIllegalArgumentException() throws Throwable {
+//    super.required_spec309_requestNegativeNumberMustSignalIllegalArgumentException();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec309_requestZeroMustSignalIllegalArgumentException() throws Throwable {
+//    super.required_spec309_requestZeroMustSignalIllegalArgumentException();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec312_cancelMustMakeThePublisherToEventuallyStopSignaling() throws Throwable {
+//    super.required_spec312_cancelMustMakeThePublisherToEventuallyStopSignaling();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec313_cancelMustMakeThePublisherEventuallyDropAllReferencesToTheSubscriber() throws Throwable {
+//    super.required_spec313_cancelMustMakeThePublisherEventuallyDropAllReferencesToTheSubscriber();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec317_mustNotSignalOnErrorWhenPendingAboveLongMaxValue() throws Throwable {
+//    super.required_spec317_mustNotSignalOnErrorWhenPendingAboveLongMaxValue();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec317_mustSupportACumulativePendingElementCountUpToLongMaxValue() throws Throwable {
+//    super.required_spec317_mustSupportACumulativePendingElementCountUpToLongMaxValue();
+//  }
+//
+//  @Test
+//  @Override
+//  public void required_spec317_mustSupportAPendingElementCountUpToLongMaxValue() throws Throwable {
+//    super.required_spec317_mustSupportAPendingElementCountUpToLongMaxValue();
+//  }
 }
