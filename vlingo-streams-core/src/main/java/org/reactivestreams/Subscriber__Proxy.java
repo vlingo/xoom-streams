cbd3a84340800bdfad073c49377bd52c15719ceb
Petru Flueras <petru.flueras@gmail.com>
2020-01-21 21:54:57 +0200

    Fixed required_spec213_blackbox_onError_mustThrowNullPointerExceptionWhenParametersAreNull test

--- vlingo-streams-core/src/main/java/org/reactivestreams/Subscriber__Proxy.java
+++ vlingo-streams-core/src/main/java/org/reactivestreams/Subscriber__Proxy.java
@@ -43,6 +43,7 @@ public class Subscriber__Proxy<T> implements org.reactivestreams.Subscriber<T> {
   }
   @Override
   public void onError(java.lang.Throwable arg0) {
+    if (arg0 == null) throw new NullPointerException("Exception must not be null.");
     if (!actor.isStopped()) {
       final java.util.function.Consumer<Subscriber> consumer = (actor) -> actor.onError(arg0);
       if (mailbox.isPreallocated()) { mailbox.send(actor, Subscriber.class, consumer, null, onErrorRepresentation3); }
