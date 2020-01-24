d7a4354b82e6e9c3eba2e0f9197389ba8bb49ad8
Petru Flueras <petru.flueras@gmail.com>
2020-01-25 01:18:16 +0200

    Cancel also the subscription when handling cancel message in StreamProcessor.
    Fixed following reactive-streams TCK tests:
    required_exerciseWhiteboxHappyPath
    required_spec208_mustBePreparedToReceiveOnNextSignalsAfterHavingCalledSubscriptionCancel

--- vlingo-streams-core/src/main/java/io/vlingo/reactivestreams/StreamSubscriberDelegate.java
+++ vlingo-streams-core/src/main/java/io/vlingo/reactivestreams/StreamSubscriberDelegate.java
@@ -74,6 +74,10 @@ public class StreamSubscriberDelegate<T> implements Subscriber<T> {
     return completed || errored;
   }
 
+  public void cancelSubscription() {
+    subscription.cancel();
+  }
+
   private void terminate() {
     sink.terminate();
   }
