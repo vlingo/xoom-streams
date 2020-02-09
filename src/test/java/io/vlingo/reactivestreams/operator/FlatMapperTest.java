8581c67978b4990dc38a94ebf4ab2f3f43110a05
Kevin Mas Ruiz <masruizkevin@gmail.com>
2020-02-09 22:04:09 +0100

    Remove unnecessary Thread.sleep

--- src/test/java/io/vlingo/reactivestreams/operator/FlatMapperTest.java
+++ src/test/java/io/vlingo/reactivestreams/operator/FlatMapperTest.java
@@ -18,14 +18,13 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class FlatMapperTest {
     @Test
-    public void testThatPropagatesRecordsFromTheProvidedSink() throws InterruptedException {
+    public void testThatPropagatesRecordsFromTheProvidedSink() {
         final Operator<Long, Long> flatMap = Operator.flatMapWith((record) -> new LongRangeSource(record, record + 2));
         final List<Long> providedLongs = new CopyOnWriteArrayList<>();
 
         flatMap.performInto(1L, providedLongs::add);
         flatMap.performInto(2L, providedLongs::add);
 
-        Thread.sleep(1000);
         Assert.assertEquals(Arrays.asList(1L, 2L, 2L, 3L), providedLongs);
     }
 }
