bea5edcc5b803d7e9fcb2d3372a83326c276fa60
VaughnVernon <vaughn@forcomprehension.com>
2020-02-17 00:26:31 +0100

    Demonstrate that flatMap() works within the Mapper.

--- src/test/java/io/vlingo/reactivestreams/operator/MapperTest.java
+++ src/test/java/io/vlingo/reactivestreams/operator/MapperTest.java
@@ -10,6 +10,9 @@ package io.vlingo.reactivestreams.operator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -32,4 +35,56 @@ public class MapperTest {
     Assert.assertEquals(456, (int) results.get(1));
     Assert.assertEquals(789, (int) results.get(2));
   }
+
+  @Test
+  public void testThatMapperFlatMapsCollections() {
+    final List<String> list1 = Arrays.asList("1", "2", "3");
+    final List<String> list2 = Arrays.asList("4", "5", "6");
+    final List<String> list3 = Arrays.asList("7", "8", "9");
+
+    final List<List<String>> lists = Arrays.asList(list1, list2, list3);
+
+    final List<Integer> results = new ArrayList<>();
+
+    final Function<List<List<String>>, List<Integer>> mapper =
+            (los) -> los.stream()
+              .flatMap(list -> list.stream().map(s -> Integer.parseInt(s)))
+              .collect(Collectors.toList());
+
+    final Operator<List<List<String>>,List<Integer>> flatMapper = Operator.mapWith(mapper);
+
+    flatMapper.performInto(lists, (numbers) -> results.addAll(numbers));
+
+    Assert.assertEquals(9, results.size());
+    Assert.assertEquals(1, (int) results.get(0));
+    Assert.assertEquals(2, (int) results.get(1));
+    Assert.assertEquals(3, (int) results.get(2));
+  }
+
+
+  @Test
+  public void testThatMapperFlatMapsOptionals() {
+    final List<Optional<String>> list1 = Arrays.asList(Optional.of("1"), Optional.of("2"), Optional.of("3"), Optional.empty());
+    final List<Optional<String>> list2 = Arrays.asList(Optional.of("4"), Optional.empty(), Optional.of("5"), Optional.of("6"));
+    final List<Optional<String>> list3 = Arrays.asList(Optional.of("7"), Optional.of("8"), Optional.empty(), Optional.of("9"));
+
+    final List<List<Optional<String>>> lists = Arrays.asList(list1, list2, list3);
+
+    final List<Optional<Integer>> results = new ArrayList<>();
+
+    final Function<List<List<Optional<String>>>, List<Optional<Integer>>> mapper =
+            (los) -> los.stream()
+              .flatMap(list -> list.stream().filter(s -> s.isPresent()).map(s -> Optional.of(Integer.parseInt(s.get()))))
+              .collect(Collectors.toList());
+
+    final Operator<List<List<Optional<String>>>,List<Optional<Integer>>> flatMapper = Operator.mapWith(mapper);
+
+    flatMapper.performInto(lists, (numbers) -> results.addAll(numbers));
+
+    Assert.assertEquals(9, results.size());
+    Assert.assertEquals(1, (int) results.get(0).get());
+    Assert.assertEquals(2, (int) results.get(1).get());
+    Assert.assertEquals(3, (int) results.get(2).get());
+  }
+
 }
